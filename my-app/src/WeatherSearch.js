1ed97d3ef4acb78154a2eb53b0ac333e491ec8dd
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-09-23 19:08:59 +0300

    added searchForm

---
+++ my-app/src/WeatherSearch.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function WeatherSearch() {
+    const [city, setCity] = useState('');
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="search" placeholder="Type a city..." onChange={updateCity} />
+            <input type="submit" value="Search" />
+        </form>
+    )
+}
